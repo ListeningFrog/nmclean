689879be154ec2e2c88da41e2d6d886e08b50e9b
Pierre Carion <pierre@rabb.it>
2018-04-04 18:07:58 -0700

    add logging

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ follow directory structure described here:
 https://github.com/golang-standards/project-layout
 
 
+To build: goreleaser --snapshot --rm-dist
+To run  : ./dist/darwin_386/nmclean 127.0.0.1
--- cmd/nmclean/main.go
+++ cmd/nmclean/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
-  "fmt"
-
+  "os"
+  // "fmt"
   "gopkg.in/alecthomas/kingpin.v2"
+  logging "github.com/op/go-logging"
 )
 
 var version = "none"
+var log = logging.MustGetLogger("nmclean")
+
+// Example format string. Everything except the message has a custom color
+// which is dependent on the log level. Many fields have a custom output
+// formatting too, eg. the time returns the hour down to the milli second.
+var format = logging.MustStringFormatter(
+	`%{color} %{level:.4s} %{id:03x}%{color:reset}> %{message}`,
+)
 
 var (
   debug   = kingpin.Flag("debug", "Enable debug mode.").Bool()
@@ -16,7 +25,10 @@ var (
 )
 
 func main() {
+  backend := logging.NewLogBackend(os.Stdout, "", 0)
+  logging.SetBackend(logging.NewBackendFormatter(backend, format))
+
   kingpin.Version(version)
   kingpin.Parse()
-  fmt.Printf("Would ping: %s with timeout %s and count %d\n", *ip, *timeout, *count)
+  log.Debugf("Would ping: %s with timeout %s and count %d\n", *ip, *timeout, *count)
 }
